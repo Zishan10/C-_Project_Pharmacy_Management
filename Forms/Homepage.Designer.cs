14c84f949f632222eac6388758e99e7d40749589
Zishan Ahmed <62927070+Zishan10@users.noreply.github.com>
2022-11-10 08:35:08 +0600

    Add files via upload

---
+++ Forms/Homepage.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace PharmacyManagement
+{
+    partial class Homepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Homepage));
+            this.homepageTitle = new System.Windows.Forms.Label();
+            this.homeLoginButton = new System.Windows.Forms.Button();
+            this.homeExitButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // homepageTitle
+            // 
+            this.homepageTitle.AutoSize = true;
+            this.homepageTitle.Font = new System.Drawing.Font("Palatino Linotype", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homepageTitle.Location = new System.Drawing.Point(123, 21);
+            this.homepageTitle.Name = "homepageTitle";
+            this.homepageTitle.Size = new System.Drawing.Size(428, 38);
+            this.homepageTitle.TabIndex = 0;
+            this.homepageTitle.Text = "Pharmacy Management System";
+            this.homepageTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // homeLoginButton
+            // 
+            this.homeLoginButton.BackColor = System.Drawing.Color.Transparent;
+            this.homeLoginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeLoginButton.Location = new System.Drawing.Point(451, 158);
+            this.homeLoginButton.Name = "homeLoginButton";
+            this.homeLoginButton.Size = new System.Drawing.Size(148, 61);
+            this.homeLoginButton.TabIndex = 1;
+            this.homeLoginButton.Text = "LOGIN";
+            this.homeLoginButton.UseVisualStyleBackColor = false;
+            this.homeLoginButton.Click += new System.EventHandler(this.homeLoginButton_Click);
+            // 
+            // homeExitButton
+            // 
+            this.homeExitButton.BackColor = System.Drawing.Color.Pink;
+            this.homeExitButton.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeExitButton.Location = new System.Drawing.Point(451, 250);
+            this.homeExitButton.Name = "homeExitButton";
+            this.homeExitButton.Size = new System.Drawing.Size(148, 61);
+            this.homeExitButton.TabIndex = 2;
+            this.homeExitButton.Text = "EXIT";
+            this.homeExitButton.UseVisualStyleBackColor = false;
+            this.homeExitButton.Click += new System.EventHandler(this.homeExitButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(359, 296);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(195)))), ((int)(((byte)(201)))));
+            this.ClientSize = new System.Drawing.Size(654, 457);
+            this.Controls.Add(this.homeLoginButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.homeExitButton);
+            this.Controls.Add(this.homepageTitle);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Homepage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Homepage";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Homepage_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label homepageTitle;
+        private System.Windows.Forms.Button homeLoginButton;
+        private System.Windows.Forms.Button homeExitButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Forms/WorkerHome.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace PharmacyManagement
+{
+    partial class WorkerHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WorkerHome));
+            this.myProfileButton = new System.Windows.Forms.Button();
+            this.inventoryButton = new System.Windows.Forms.Button();
+            this.createBillButton = new System.Windows.Forms.Button();
+            this.billHistoryButton = new System.Windows.Forms.Button();
+            this.workerLogOutButton = new System.Windows.Forms.Button();
+            this.workerHomeSideBarLabel = new System.Windows.Forms.Label();
+            this.workerHomeSidePanel = new System.Windows.Forms.Panel();
+            this.myIssueButton = new System.Windows.Forms.Button();
+            this.issueButton = new System.Windows.Forms.Button();
+            this.homeIdLabel = new System.Windows.Forms.Label();
+            this.WorkerHomePanel = new System.Windows.Forms.Panel();
+            this.WorkerPanelClearButton = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.workerHomeSidePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myProfileButton
+            // 
+            this.myProfileButton.BackColor = System.Drawing.Color.Transparent;
+            this.myProfileButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.myProfileButton.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myProfileButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.myProfileButton.Location = new System.Drawing.Point(24, 170);
+            this.myProfileButton.Margin = new System.Windows.Forms.Padding(4);
+            this.myProfileButton.Name = "myProfileButton";
+            this.myProfileButton.Size = new System.Drawing.Size(130, 46);
+            this.myProfileButton.TabIndex = 1;
+            this.myProfileButton.Text = "My Profile";
+            this.myProfileButton.UseVisualStyleBackColor = false;
+            this.myProfileButton.Click += new System.EventHandler(this.myProfileButton_Click);
+            // 
+            // inventoryButton
+            // 
+            this.inventoryButton.BackColor = System.Drawing.Color.Transparent;
+            this.inventoryButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("inventoryButton.BackgroundImage")));
+            this.inventoryButton.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inventoryButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.inventoryButton.Location = new System.Drawing.Point(24, 238);
+            this.inventoryButton.Margin = new System.Windows.Forms.Padding(4);
+            this.inventoryButton.Name = "inventoryButton";
+            this.inventoryButton.Size = new System.Drawing.Size(130, 49);
+            this.inventoryButton.TabIndex = 2;
+            this.inventoryButton.Text = "Inventory";
+            this.inventoryButton.UseVisualStyleBackColor = false;
+            this.inventoryButton.Click += new System.EventHandler(this.inventoryButton_Click);
+            // 
+            // createBillButton
+            // 
+            this.createBillButton.BackColor = System.Drawing.Color.Transparent;
+            this.createBillButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("createBillButton.BackgroundImage")));
+            this.createBillButton.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createBillButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.createBillButton.Location = new System.Drawing.Point(24, 306);
+            this.createBillButton.Margin = new System.Windows.Forms.Padding(4);
+            this.createBillButton.Name = "createBillButton";
+            this.createBillButton.Size = new System.Drawing.Size(130, 45);
+            this.createBillButton.TabIndex = 3;
+            this.createBillButton.Text = "Create Bill";
+            this.createBillButton.UseVisualStyleBackColor = false;
+            this.createBillButton.Click += new System.EventHandler(this.createBillButton_Click);
+            // 
+            // billHistoryButton
+            // 
+            this.billHistoryButton.BackColor = System.Drawing.Color.Transparent;
+            this.billHistoryButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("billHistoryButton.BackgroundImage")));
+            this.billHistoryButton.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.billHistoryButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.billHistoryButton.Location = new System.Drawing.Point(24, 369);
+            this.billHistoryButton.Margin = new System.Windows.Forms.Padding(4);
+            this.billHistoryButton.Name = "billHistoryButton";
+            this.billHistoryButton.Size = new System.Drawing.Size(130, 45);
+            this.billHistoryButton.TabIndex = 5;
+            this.billHistoryButton.Text = "Bill History";
+            this.billHistoryButton.UseVisualStyleBackColor = false;
+            this.billHistoryButton.Click += new System.EventHandler(this.billHistoryButton_Click);
+            // 
+            // workerLogOutButton
+            // 
+            this.workerLogOutButton.BackColor = System.Drawing.Color.DarkRed;
+            this.workerLogOutButton.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.workerLogOutButton.ForeColor = System.Drawing.Color.FloralWhite;
+            this.workerLogOutButton.Location = new System.Drawing.Point(24, 593);
+            this.workerLogOutButton.Margin = new System.Windows.Forms.Padding(4);
+            this.workerLogOutButton.Name = "workerLogOutButton";
+            this.workerLogOutButton.Size = new System.Drawing.Size(130, 46);
+            this.workerLogOutButton.TabIndex = 6;
+            this.workerLogOutButton.Text = "Log Out";
+            this.workerLogOutButton.UseVisualStyleBackColor = false;
+            this.workerLogOutButton.Click += new System.EventHandler(this.workerLogOutButton_Click);
+            // 
+            // workerHomeSideBarLabel
+            // 
+            this.workerHomeSideBarLabel.AutoSize = true;
+            this.workerHomeSideBarLabel.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.workerHomeSideBarLabel.Font = new System.Drawing.Font("Vineta BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.workerHomeSideBarLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.workerHomeSideBarLabel.Location = new System.Drawing.Point(19, 9);
+            this.workerHomeSideBarLabel.Name = "workerHomeSideBarLabel";
+            this.workerHomeSideBarLabel.Size = new System.Drawing.Size(146, 26);
+            this.workerHomeSideBarLabel.TabIndex = 7;
+            this.workerHomeSideBarLabel.Text = "WORKER";
+            this.workerHomeSideBarLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // workerHomeSidePanel
+            // 
+            this.workerHomeSidePanel.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.workerHomeSidePanel.Controls.Add(this.pictureBox3);
+            this.workerHomeSidePanel.Controls.Add(this.myIssueButton);
+            this.workerHomeSidePanel.Controls.Add(this.issueButton);
+            this.workerHomeSidePanel.Controls.Add(this.myProfileButton);
+            this.workerHomeSidePanel.Controls.Add(this.workerLogOutButton);
+            this.workerHomeSidePanel.Controls.Add(this.homeIdLabel);
+            this.workerHomeSidePanel.Controls.Add(this.workerHomeSideBarLabel);
+            this.workerHomeSidePanel.Controls.Add(this.billHistoryButton);
+            this.workerHomeSidePanel.Controls.Add(this.createBillButton);
+            this.workerHomeSidePanel.Controls.Add(this.inventoryButton);
+            this.workerHomeSidePanel.Location = new System.Drawing.Point(0, -7);
+            this.workerHomeSidePanel.Name = "workerHomeSidePanel";
+            this.workerHomeSidePanel.Size = new System.Drawing.Size(200, 781);
+            this.workerHomeSidePanel.TabIndex = 0;
+            this.workerHomeSidePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.workerHomeSidePanel_Paint);
+            // 
+            // myIssueButton
+            // 
+            this.myIssueButton.BackColor = System.Drawing.Color.Transparent;
+            this.myIssueButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("myIssueButton.BackgroundImage")));
+            this.myIssueButton.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myIssueButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.myIssueButton.Location = new System.Drawing.Point(24, 511);
+            this.myIssueButton.Margin = new System.Windows.Forms.Padding(4);
+            this.myIssueButton.Name = "myIssueButton";
+            this.myIssueButton.Size = new System.Drawing.Size(130, 46);
+            this.myIssueButton.TabIndex = 11;
+            this.myIssueButton.Text = "My Issues";
+            this.myIssueButton.UseVisualStyleBackColor = false;
+            this.myIssueButton.Click += new System.EventHandler(this.myIssueButton_Click);
+            // 
+            // issueButton
+            // 
+            this.issueButton.BackColor = System.Drawing.Color.Transparent;
+            this.issueButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("issueButton.BackgroundImage")));
+            this.issueButton.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.issueButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.issueButton.Location = new System.Drawing.Point(24, 441);
+            this.issueButton.Margin = new System.Windows.Forms.Padding(4);
+            this.issueButton.Name = "issueButton";
+            this.issueButton.Size = new System.Drawing.Size(130, 46);
+            this.issueButton.TabIndex = 10;
+            this.issueButton.Text = "Create Issue";
+            this.issueButton.UseVisualStyleBackColor = false;
+            this.issueButton.Click += new System.EventHandler(this.issueButton_Click);
+            // 
+            // homeIdLabel
+            // 
+            this.homeIdLabel.AutoSize = true;
+            this.homeIdLabel.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.homeIdLabel.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeIdLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.homeIdLabel.Location = new System.Drawing.Point(61, 46);
+            this.homeIdLabel.Name = "homeIdLabel";
+            this.homeIdLabel.Size = new System.Drawing.Size(53, 33);
+            this.homeIdLabel.TabIndex = 9;
+            this.homeIdLabel.Text = "100";
+            this.homeIdLabel.Click += new System.EventHandler(this.homeIdLabel_Click);
+            // 
+            // WorkerHomePanel
+            // 
+            this.WorkerHomePanel.BackColor = System.Drawing.Color.Transparent;
+            this.WorkerHomePanel.Font = new System.Drawing.Font("Microsoft Tai Le", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WorkerHomePanel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.WorkerHomePanel.Location = new System.Drawing.Point(206, 2);
+            this.WorkerHomePanel.Name = "WorkerHomePanel";
+            this.WorkerHomePanel.Size = new System.Drawing.Size(1374, 641);
+            this.WorkerHomePanel.TabIndex = 9;
+            this.WorkerHomePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.WorkerHomePanel_Paint);
+            // 
+            // WorkerPanelClearButton
+            // 
+            this.WorkerPanelClearButton.BackColor = System.Drawing.Color.LightPink;
+            this.WorkerPanelClearButton.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WorkerPanelClearButton.Location = new System.Drawing.Point(1533, 651);
+            this.WorkerPanelClearButton.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.WorkerPanelClearButton.Name = "WorkerPanelClearButton";
+            this.WorkerPanelClearButton.Size = new System.Drawing.Size(36, 34);
+            this.WorkerPanelClearButton.TabIndex = 54;
+            this.WorkerPanelClearButton.Text = "X";
+            this.WorkerPanelClearButton.UseVisualStyleBackColor = false;
+            this.WorkerPanelClearButton.Click += new System.EventHandler(this.InventoryCloseButton_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(53, 82);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 76);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 61;
+            this.pictureBox3.TabStop = false;
+            // 
+            // WorkerHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1584, 693);
+            this.Controls.Add(this.WorkerPanelClearButton);
+            this.Controls.Add(this.WorkerHomePanel);
+            this.Controls.Add(this.workerHomeSidePanel);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "WorkerHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WorkerHome";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.WorkerHome_FormClosed);
+            this.Load += new System.EventHandler(this.WorkerHome_Load);
+            this.workerHomeSidePanel.ResumeLayout(false);
+            this.workerHomeSidePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button myProfileButton;
+        private System.Windows.Forms.Button inventoryButton;
+        private System.Windows.Forms.Button createBillButton;
+        private System.Windows.Forms.Button billHistoryButton;
+        private System.Windows.Forms.Button workerLogOutButton;
+        private System.Windows.Forms.Label workerHomeSideBarLabel;
+        private System.Windows.Forms.Panel workerHomeSidePanel;
+        private System.Windows.Forms.Label homeIdLabel;
+        private System.Windows.Forms.Panel WorkerHomePanel;
+        private System.Windows.Forms.Button WorkerPanelClearButton;
+        private System.Windows.Forms.Button issueButton;
+        private System.Windows.Forms.Button myIssueButton;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
